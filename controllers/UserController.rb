f9893ea222987075c1ebf38b98e90f0e3ab949c5
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-02-24 17:20:46 -0600

    can create users

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -1,8 +1,22 @@
 class UserController < ApplicationController 
 
+
+	get "/" do 
+
+		erb :register
+
+	end
+
+
 	post "/" do 
 
-		
+		user = User.new 
+
+		user.username = params[:username]
+
+		user.save 
+
+		redirect "/"
 
 	end
 
